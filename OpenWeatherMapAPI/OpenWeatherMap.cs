20a423eaf21769edad5ad79de82d5311cd05d8bd
JustinBiga <BISENGOJ@GMAIL.COM>
2024-04-29 05:31:09 -0400

    Init commit

---
+++ OpenWeatherMapAPI/OpenWeatherMap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI
+{
+	public class OpenWeatherMap
+	{
+        private HttpClient _httpClient;
+        private string _apiKey;
+
+		public OpenWeatherMap(string apiKey)
+		{
+            _httpClient = new HttpClient();
+			_apiKey = apiKey;
+
+		}
+        public string GetWeather(string city)
+        {
+         HttpResponseMessage response = _httpClient.GetAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={_apiKey}&units=imperial").Result;
+
+         string response1 = response.Content.ReadAsStringAsync().Result;
+         return response1;
+           
+        }
+
+        public string GetCurrentWeather(string city)
+        {
+            string response1 = GetWeather(city);
+
+            JObject json = JObject.Parse(response1);
+
+            string cityName = (string)json["name"];
+
+            double temperature = (double)json["main"]["temp"];
+
+            string weatherDescription = (string)json["weather"][0]["description"];
+
+            return $"Weather in {cityName}:\nTemperature: {temperature}°F\nDescription: {weatherDescription}";
+        }
+
+    }
+}
+
---
+++ OpenWeatherMapAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenWeatherMapAPI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Insert your OpenWeatherMap API key here
+        string apiKey = "7ce910b3916abcc7c4b9c52fb9fe8f47";
+
+        // Specify the city for which you want to get the weather
+        string city = "New York";
+
+        // Create an instance of OpenWeatherMapClient
+        OpenWeatherMap client = new OpenWeatherMap(apiKey);
+
+        // Get the weather information for the specified city
+        string weatherInfo = client.GetCurrentWeather(city);
+
+        // Display the weather information
+        Console.WriteLine(weatherInfo);
+
+    }
+}
+
+
+
+
